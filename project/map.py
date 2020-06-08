a6736ee25991d3c06ccb4555d0a23205e9f4e1ab
taewonyang <tw_24@naver.com>
2020-06-08 23:38:54 +0900

    new

--- project/map.py
+++ project/map.py
@@ -35,4 +35,4 @@ for addr in address_list :
 m = folium.Map(location=[37.4838699,127.0565831], zoom_start=12)
 for x_code, y_code, name in zip(x_code_list, y_code_list, name_list):
     folium.Marker(location=[y_code, x_code], tooltip=name, icon=folium.Icon(color ="red", icon="star")).add_to(m)
-m.save('map.html')
+m.save('index.html')
