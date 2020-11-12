8ee6252e50c90d00dd4d07e563cce595a9a58601
taewonyang <tw_24@naver.com>
2020-11-12 22:35:00 +0900

    기능개선
    1.역 검색시, 해당지역으로 지도 이동
    2. 데이터관리 - 상호명 검색시
    : 자료 없을시 알람창설정

--- project/app.py
+++ project/app.py
@@ -164,14 +164,13 @@ def crawling():
     return jsonify({'crawling_result': crawling_result, 'stores': stores})
 
 
-@app.route('/revise', methods=['get'])
-def revise():
+@app.route('/reviseMainPage', methods=['get']) #데이터관리 메인페이지
+def reviseMainPage():
     storeName_receive = request.args.get('storeName_give')
-    print(storeName_receive)
     if storeName_receive=="" :
        store_info = list(db.food_store.find({}, {'_id': 0}))
        result = "success"
-    else :
+    else : #데이터관리 페이지에서 검색시
         store_info = []
         all = list(db.food_store.find({}, {'_id': 0}))
         for i in all :
@@ -181,19 +180,74 @@ def revise():
                 for store in stores :
                     store_info.append(store)
                 result = "success"
+            else :
+                result = "fail"
     return jsonify({'result':result, 'store_info':store_info})
 
-# @app.route('/revise', methods=['post'])
-# def revise():
-    # stationBox_receive = request.args.get('stationBox_give')
-    # if stationBox_receive=="" :
-    #    store_info = list(db.food_store.find({}, {'_id': 0}))
-    #    result = "success"
-    # else :
-    #     store_info = list(db.food_store.find({'station':stationBox_receive}, {'_id': 0}))
-    #     result = "success"
-    # return jsonify({'result':result, 'store_info':store_info})
-    # return jsonify({'result': 'result'})
+
+@app.route('/revise_changeboxListing', methods=['get']) #데이터관리 - 데이터수정창
+def revise_changeboxListing() :
+    store_receive = request.args.get('store_give')
+    print(store_receive,'수정창내용')
+    stores = list(db.food_store.find({'name': store_receive}, {'_id': 0}))
+    store_info = []
+    for store in stores :
+        store_info.append(store)
+    result = "success"
+    return jsonify({'result':result, 'store_info':store_info})
+
+
+@app.route('/DataUpdate', methods=['post'])
+def DataUpdate():
+    no_receive = request.form['no_give']
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    station_receive = request.form['station_give']
+    visit_receive = request.form['visit_give']
+    food_kind_receive = request.form['food_kind_give']
+    memo_receive = request.form['memo_give']
+
+    if ((station_receive in stationList) == True):
+        # 위도,경도 추출하기
+        api_key = '340094C7-00E9-3582-9DDE-6566D6A8605C'
+        api_url = f'http://api.vworld.kr/req/address?service=address&request=getcoord&version=2.0&crs=epsg:4326&address={address_receive}&refine=true&simple=false&format=json&type=ROAD&key={api_key}'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
+        res = requests.get(api_url, headers=headers)
+        html = res.content.decode('utf8')
+        x_code = re.findall('"x" : "(.*?)", "y', html)[0]
+        y_code = re.findall('"y" : "(.*?)"}', html)[0]
+
+        doc = {'no': no_receive, 'name': name_receive, 'address': address_receive, 'x_code': x_code, 'y_code': y_code,
+               'station': station_receive, 'memo': memo_receive, 'visit': visit_receive,
+               'food_kind': food_kind_receive}
+
+        print(name_receive, '입력받은값')
+        db.food_store.update_one( { 'no' : no_receive }, doc )  #update is deprecated. Use replace_one, update_one or update_many instead
+        print(list(db.food_store.find({'no': no_receive}, {'_id': 0})), '변경된내용')
+
+        # # 데이터 수정한 지도만들기
+        # m = folium.Map(location=[37.529471, 127.008920], zoom_start=12)
+        # all_store = list(db.food_store.find({}))
+        # for store in all_store:
+        #     name = store['name']
+        #     visit = store['visit']
+        #     x_code = store['x_code']
+        #     y_code = store['y_code']
+        #     if visit == "visit_ok":
+        #         folium.Marker(location=[y_code, x_code], tooltip=name,
+        #                       icon=folium.Icon(color="blue", icon="home")).add_to(m)
+        #     else:
+        #         folium.Marker(location=[y_code, x_code], tooltip=name,
+        #                       icon=folium.Icon(color="red", icon="star")).add_to(m)
+        # m.save('./templates/map.html')
+        # print('지도화 성공!')
+        result = "success"
+    else :
+        result = 'fail'
+        print('잘못된 역명칭 입력!')
+
+    return jsonify({'result': result})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
